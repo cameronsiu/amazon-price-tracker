ff1673b138f20c633ca10d33ec40d8aa60091129
Cameron Siu <cameronsiu02@gmail.com>
2022-09-27 02:13:16 -0700

    "added mongoDB functions"

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const chrome = require("selenium-webdriver/chrome");
 
@@ -19,19 +18,39 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use('/', indexRouter);
-app.get('/getData', (req, res) => {
+
+let debug = 0;
+app.get('/getData', async (req, res) => {
   //res.header("Access-Control-Allow-Origin", "*");
   //console.log(req);
-  res.json({ msg: "Message delivered!"});
+  debug += 1;
+  console.log(debug);
+  const prods = await run().catch(console.dir);
+  res.json({ msg: "Message delivered!", products: prods});
+  
 });
 
-app.post('/trackPrice', (req, res) => {
+app.post('/trackPrice', async (req, res) => {
   //console.log(req);
   console.log("BODY IS: ", req.body['URL']);
   //console.log(typeof(req.body));
   const url = req.body['URL'];
-  //build(url);
+  const price = await build(url);
+  const name = url.split('https://www.amazon.ca/')[1].split('/dp/')[0];
+  req.body['name'] = name;
+  // console.log(price);
+  req.body['price'] = price;
+  await insertProduct(req.body);
+  console.log(req.body);
   res.json(req.body);
+})
+
+app.post('/deletePrice', async (req, res) => {
+  //console.log(req);
+  console.log("BODY IS: ", req.body);
+  //console.log(typeof(req.body));
+  await deleteProduct(req.body['_id']);
+  res.json("Deleted item!");
 })
 
 // catch 404 and forward to error handler
@@ -39,51 +58,75 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+//TODO: Build should be its own separate file
 const service = new chrome.ServiceBuilder('./chromedriver.exe');
 async function build(URL) {
-  //let driver = await new Builder().forBrowser('chrome').setChromeService(service).build();
-
   let driver = await new Builder().forBrowser('chrome').setChromeService(service).setChromeOptions(new chrome.Options().headless().addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")).build();
   //await driver.get('https://www.amazon.ca/LG-27GL83A-B-27-Inch-Led-Lit-14700510/dp/B07YGZL8XF');
   await driver.get(URL);
-  //const source = await driver.getPageSource();
-  //console.log(source);
-  //const price_promise = await driver.wait(until.elementLocated(By.className('a-price-whole')), 30000, 'Timed out after 30 seconds', 5000);
-  const price_promise = await driver.findElement(By.className('a-price-whole'));
-  const price_text = await price_promise.getText();
-  console.log(price_text);
-  const price_fraction_promise = await driver.findElement(By.className('a-price-fraction'));
-  const price_fraction_text = '.' + (await price_fraction_promise.getText());
-  console.log(price_fraction_text);
-  const int_price = parseFloat(price_text + price_fraction_text);
-  console.log(int_price);
-  
+
+  const price_locations = [By.className('a-price'), By.id('price')];
+
+  for (price_location of price_locations) {
+    let price = await driver.findElement(price_location).getText();
+    if (price !== "") { // If true, element exists, otherwise element doesn't exist.
+      price = price.split('\n').join('.');
+      console.log(`Debug: (${price})`);
+      await driver.quit();
+      return price;
+    }
+  }
+
   await driver.quit();
+  return "Cannot find price";
 };
 
+/*
+Example links:
+https://www.amazon.ca/Real-Book-6th-Leonard-Corp/dp/0634060384 - id price
+https://www.amazon.ca/LG-UltraGear-34GP83A-B-Adaptive-Sync-Compatible/dp/B08DWD38VX - className a-price
+*/
 
-const { MongoClient } = require("mongodb");
+//TODO: Connecting to MongoDB should be its own separate file
+const { MongoClient, ObjectId } = require("mongodb");
 
 // Connection URI
 const uri = "mongodb+srv://csiu02:1221@cluster0.558obql.mongodb.net/?retryWrites=true&w=majority";
 // Create a new MongoClient
 const client = new MongoClient(uri);
+console.log("Connected successfully to server");
+
 
 async function run() {
+  const result = [];
   try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    const database = client.db("users");
+    const products = database.collection("cameron").find(); // change this to user name or user_id
+    await products.forEach(doc => result.push(doc));
+    console.log("RUN FUNCTION RESULT: ", result);
+    return result;
+  } catch (err) {
+    console.log(err);
   }
 }
-run().catch(console.dir);
 
+async function insertProduct(product) {
+  try {
+    const database = client.db("users");
+    const products = await database.collection("cameron").insertOne(product); // change this to user name or user_id
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+async function deleteProduct(productId) {
+  try {
+    const database = client.db("users");
+    const products = await database.collection("cameron").deleteOne({_id: ObjectId(productId)}); // change this to user name or user_id
+    console.log(products);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 module.exports = app;
--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -1,24 +1,40 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+type Product = {_id: any, name: string, price: string, URL: string};
 
 export default defineComponent ({
   data() {
     return {
-      products: [] as Array<string>,
+      products: [] as Array<Product>,
       link: ""
     }
   },
+  beforeMount() {
+      this.getData()
+  },
   methods: {
+    getData() {
+      if (!window.localStorage.getItem("products")) {
+        console.log("Fetching data...");
+        fetch('http://localhost:3000/getData')
+        .then((res) => res.json())
+        .then(data => {
+          console.log(data['products'])
+          data['products'].forEach((product: Product) => {
+            this.products.push(product);
+          });
+          window.localStorage.setItem("products", JSON.stringify(this.products));
+        })
+        .catch(err => {
+          console.log("ERROR: ", err);
+        }); 
+      } else {
+        this.products = JSON.parse(window.localStorage.getItem("products")!); // ! - non-null assertion
+      }
+    },
     submit() {
       // Enters the link into the database and starts tracking
       console.log(this.link);
-      fetch('http://localhost:3000/getData')
-      .then((res) => res.json())
-      .then(data => console.log(data))
-      .catch(err => {
-        console.log("ERROR: ", err);
-      });
-      
       fetch('http://localhost:3000/trackPrice', {
         method: 'POST',
         headers: {
@@ -32,15 +48,38 @@ export default defineComponent ({
       .then((res) => res.json())
       .then((data: any) => {
         this.products.push(data);
+        window.localStorage.setItem("products", JSON.stringify(this.products));
+        console.log(data);
       })
       .catch(err => {
         console.log("ERROR: ", err);
       });
       
     },
-    remove_product(product: any) {
-      this.products.splice(this.products.indexOf(product), 1);
-      // make call to backend
+    async remove_product(product: Product) {
+      // TODO: As an exercise, create my own popup
+      if (confirm("Are you sure you want to delete this product?")) {
+        this.products.splice(this.products.indexOf(product), 1);
+        //window.localStorage.setItem("products", JSON.stringify(this.products));
+        // make call to backend
+        const deleted_data = await fetch('http://localhost:3000/deletePrice', 
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({_id: product._id})
+          }
+        ).then((res) => res.json());
+        console.log(deleted_data);
+        // Fetch the data from database
+        const data = await fetch('http://localhost:3000/getData').then((res) => res.json());
+        console.log(data['products']);
+        this.products = data['products'];
+        window.localStorage.setItem("products", JSON.stringify(this.products));
+       
+      }
+      
     }
   }
 })
@@ -49,7 +88,7 @@ export default defineComponent ({
 <template>
   <div class="input-group mb-3 d-flex p-2 bd-highlight">
     <input v-model="link" type="text" class="form-control" placeholder="Add Amazon Product URL"  aria-label="Add Amazon Product URL" aria-describedby="button-addon2" >
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="submit">Track</button>
+    <button class="btn btn-secondary" type="button" id="button-addon2" @click="submit">Track</button>
   </div>
   <div class="container">
     <ul class="list-group d-flex p-2 bd-highlight">
